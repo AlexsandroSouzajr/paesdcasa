53d5c7593ca48871a9f9915c3efacf33335ba7ff
AlexsandroSouzajr <alexsandrosouzajr10@gmail.com>
2023-10-19 14:52:34 -0300

    atualizado

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -262,3 +262,22 @@ const options = {
 };
 
 const animation = new AnimateOnScroll(options);
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
