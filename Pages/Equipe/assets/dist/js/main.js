c9d083e2b5960d63869f0d7f12afdf9daf87ecfa
AlexsandroSouzajr <alexsandrosouzajr10@gmail.com>
2023-11-16 14:48:29 -0300

    atualizado

--- Pages/Equipe/assets/dist/js/main.js
+++ Pages/Equipe/assets/dist/js/main.js
@@ -14,9 +14,39 @@ const body = document.querySelector("body"),
   aboutSection = document.querySelector(".about"),
   footerSection = document.querySelector("footer");
 
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({ pageLanguage: 'pt' }, 'google_translate_element');
-}
+// function googleTranslateElementInit() {
+//   new google.translate.TranslateElement({ pageLanguage: 'pt' }, 'google_translate_element');
+// }
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+
+  
+select.addEventListener('click', () => {
+  select.classList.toggle('select-clicked');
+  caret.classList.toggle('caret-rotate');
+  menu.classList.toggle('menu-open');
+});
+
+options.forEach(option => {
+  option.addEventListener('click', () => {
+      selected.innerText = option.innerText;
+      select.classList.remove('select-clicked');
+      caret.classList.remove('caret-rotate');
+      menu.classList.remove('menu-open');
+    
+      options.forEach(option => {
+        option.classList.remove('active');
+        });
+          option.classList.add('active');
+  });
+});
+});
 
 const modeToggle = document.querySelector(".dark-light");
 
