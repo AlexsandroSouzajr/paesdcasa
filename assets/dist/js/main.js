5c29112a10506061c5c42ecc51bdd0b3b8c12887
AlexsandroSouzajr <alexsandrosouzajr10@gmail.com>
2023-12-11 16:50:18 -0300

    atualizado

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -472,18 +472,3 @@ mainTl.from(['.treePathMask','.treePotMask'],{
 
 mainTl.add(starTl, 0)
 gsap.globalTimeline.timeScale(1.5);
-
-
-// typical import
-import gsap from "gsap";
-
-// get other plugins:
-import ScrollTrigger from "gsap/ScrollTrigger";
-import Flip from "gsap/Flip";
-import Draggable from "gsap/Draggable";
-
-// or all tools are exported from the "all" file (excluding members-only plugins):
-import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "gsap/all";
-
-// don't forget to register plugins
-gsap.registerPlugin(ScrollTrigger, Draggable, Flip, MotionPathPlugin); 
