a050675ddc43a78bc6304f9b6aec5769e88b05b4
AlexsandroSouzajr <alexsandrosouzajr10@gmail.com>
2024-02-13 23:59:37 -0300

    updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -299,3 +299,34 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   }).mount();
 });
+
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  centerSlide: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },
+});
